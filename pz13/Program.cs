dfa843c7419e15267cca0902aefb3bbb1381282c
Павел Левченков <levchenkov.levp@mail.ru>
2022-12-21 10:31:52 +0500

    pz13

---
+++ pz13/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace pz13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetNumbers(new double[] { 1.2, 3.4, 5.6 }, 2));
+        }
+
+        static int GetNumbers(double[] nums, int n)
+        {
+            int count = 0;
+
+            foreach (double d in nums)
+            {
+                if (d > n)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
